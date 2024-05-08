ac90c07ef7886910a885f4739ddb666e7385a291
Judhunja <judxhunja@gmail.com>
2024-05-08 17:13:58 +0300

    Basic dictionary for caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""This module contains a class BasicCache"""
+
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    Implements a caching system with no limits
+    """
+    """def __init__(self):
+        """"Initializes BasicCache""""
+        self.cache_data = super().cache_data"""
+
+    def put(self, key, item):
+        """Assigns self.chache_data the item value for key"""
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Returns value in self.cache_data for key"""
+        if key is None:
+            return None
+        else:
+            return self.cache_data.get(key, None)
---
+++ 0x01-caching/README.md
@@ -0,0 +1,5 @@
+## 0x01. Caching
+- Caching system
+- FIFO, LIFO, LRU, MRU, LFU
+- Purpose of a caching system
+- What limits a caching system have
