155b14f4ae81e9a729887f7211f43de2f92913f4
Judhunja <judxhunja@gmail.com>
2024-05-22 16:03:33 +0300

    simple flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""This module contains a basic flask app"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """Route for the home page"""
+    return render_template('index.html')
