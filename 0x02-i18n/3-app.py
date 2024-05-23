d238c93073b745d5476a46468c2145b217dac16b
Judhunja <judxhunja@gmail.com>
2024-05-23 14:27:28 +0300

    parameterizing templates with the correct language as requested by the client

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""This module contains a basic flask app"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Config for this flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def home():
+    """Route for the home page"""
+    locale = get_locale()
+    welcome_msg = _('home_title')
+    header = _('home_header')
+    return render_template('3-index.html',
+                           locale=locale,
+                           home_title=welcome_msg,
+                           home_header=header)
+
+
+@babel.localeselector
+def get_locale():
+    """Determines the best match of this application
+    with the client requested languages"""
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
