a9eec4de550e9ecf64e04eeaab4df4240f7e20b4
Judhunja <judxhunja@gmail.com>
2024-06-13 21:35:31 +0300

    creating node redis client

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,17 @@
+import { createClient } from 'redis';
+
+async function redisConn() {
+        const client = createClient();
+        client.on('error', err => {
+                console.log(`Redis client not connected to the server: ${err}`)
+        });
+
+        try {
+                await client.connect();
+                console.log('Redis client connected to the server');
+        } catch (err) {
+                console.log(`Redis client not connected to the server: ${err}`);
+        }
+};
+
+redisConn();
