5c8f39869b6dc88e097d8fcd2cadab8a81a98033
Judhunja <judxhunja@gmail.com>
2024-07-31 18:26:47 +0300

    job creator using kue

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,24 @@
+const kue = require('kue');
+
+const queue = kue.createQueue();
+
+const jobDetails = {
+  phoneNumber: '55555555',
+  message: 'Welcome'
+};
+
+const job = queue.create('push_notification_code', job).save((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Notification job created: ${job.id}`);
+  }
+});
+
+job.on('complete', (result) => {
+  console.log('Notification job completed');
+});
+
+job.on('failed', (err) => {
+  console.log('Notification job failed');
+});
